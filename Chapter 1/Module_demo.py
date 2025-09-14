dc70c1e571e291d0f4f2155f06c7437b0bdcb9c9
pinu <pinudash104@gmail.com>
2025-09-14 12:46:36 +0530

    Chapter 1

---
+++ Chapter 1/Module_demo.py
@@ -0,0 +1,10 @@
+import os
+
+
+from random import *
+
+
+
+import numpy,cv2
+
+print(randrange(0,100))
---
+++ Chapter 1/comments.py
@@ -0,0 +1,14 @@
+print("Hello, World!") # This line will just print Hello, World!
+
+#Below line will print just Hii
+print("Hii")
+
+""" 
+This is an
+example of 
+multilines
+comments
+and below will give 
+output of 5+6
+"""
+print(5+6)
