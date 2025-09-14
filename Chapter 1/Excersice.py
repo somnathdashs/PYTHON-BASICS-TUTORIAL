9365d86ec9c4ec253a8f8e5e82127d710be09f1e
pinu <pinudash104@gmail.com>
2025-09-14 13:07:52 +0530

    chapter 1 solution

---
+++ Chapter 1/Excersice.py
@@ -0,0 +1,12 @@
+# Q1
+print(70+30)
+
+
+#Q2
+import numpy as np
+
+print(np.asin(0.5))
+
+#Q3
+import os #We are importing os module to our program
+print(os.listdir("./Chapter 1/")) # We are printing the file name in folder Chapter 1
